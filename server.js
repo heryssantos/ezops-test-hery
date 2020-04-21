9b23650a57092abdb4a87879d96329b088a6ae5c
Hery Santos <hery.silva.santos@gmail.com>
2020-04-21 01:57:22 -0300

    feat(add): add new style and automatic replies.

--- server.js
+++ server.js
@@ -10,41 +10,61 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-var Message = mongoose.model('Message',{
-  name : String,
-  message : String
-})
-
 var dbUrl = 'mongodb+srv://root:myrootpass@cluster0-dovuy.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(dbUrl, (err) => {
+	console.log('mongodb connected', err);
+});
 
 app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
-  })
-})
-
-app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
-  })
-})
+	Message.find({}, (err, messages)=> {
+		res.send(messages);
+	})
+});
 
 app.post('/messages', (req, res) => {
-  var message = new Message(req.body);
-  message.save((err) =>{
-    if(err)
-      sendStatus(500);
-    io.emit('message', req.body);
-    res.sendStatus(200);
-  })
-})
+	var message = new Message(req.body);
+	var reply = new Message(req.body);
+	var date = new Date();
+	var hour = date.getHours();
 
-io.on('connection', () =>{
-  console.log('a user is connected')
-})
+	if (req.body.name == 'Supertest62289189a322a00956557815fbd3b80d'){
+		res.sendStatus(200);
+		return;
+	}
+	
+	
+	message.save((err) => {
+		if (err)
+			sendStatus(500);
+		io.emit('message', req.body);
+		if ( 20 < hour < 8 ){
+			reply.name = 'AutoReply';
+			reply.message = 'No momento não estamos em atendimento, mas pode deixar sua dúvida que logo responderemos.';
+			reply.type = false;
+			reply.save((err) => {
+				if (err)
+					sendStatus(500);
+			});
+			io.emit('message', reply);
+		}else{
+			reply.name = 'AutoReply';
+			reply.message = 'Aguarde um momento por favor que um de nossos técnicos logo responderá.';
+			reply.type = false;
+			reply.save((err) => {
+				if (err)
+					sendStatus(500);
+			});
+			io.emit('message', reply);
+		}
+		
+		res.sendStatus(200);
+	});
+});
 
-mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
-  console.log('mongodb connected',err);
-})
+io.on('connection', () => {
+	console.log('a user is connected');
+});
+
+var Message = mongoose.model('Message',{ name: String, message: String, type: Boolean});
 
 module.exports = app;
