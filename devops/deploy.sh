f5f0307b837642c311c125355961eeb7ad2d22da
Hery Santos <hery_eu_@hotmail.com>
2020-05-07 18:54:20 -0300

    chore(app): fix path of test folder and files

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 echo "Pulling repository"
-git pull
+#git pull
+echo "Testing ..."
+bash test
 echo "Stoping container"
 docker container stop node-container
 echo "Removing container"
 docker container rm node-container
 docker-compose up -d node
-
